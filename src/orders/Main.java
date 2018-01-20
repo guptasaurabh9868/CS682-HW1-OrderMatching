f8b3251fd38b7372ba9627af81c08e8ad0e63413
SAURABH GUPTA <guptas@cse.iitb.ac.in>
2018-01-20 10:38:38 +0530

    Read Function added.

--- src/orders/Main.java
+++ src/orders/Main.java
@@ -3,10 +3,36 @@
  */
 package orders;
 
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
 /**
  * @author guptas
  *
  */
+class Orders{
+	
+	String ord_ID;
+	String type;
+	String product;
+	String pro_ID;
+	int price;
+	int qty;
+	String cust_ID;
+	
+	public void read(Orders O)
+	{
+		Scanner s = new Scanner(System.in);
+		this.ord_ID = s.next();
+		this.type = s.next();
+		this.product = s.nextLine();
+		this.pro_ID  = s.nextLine();
+		this.price = s.nextInt();
+		this.qty = s.nextInt();
+		this.cust_ID = s.nextLine();
+	}
+}
 public class Main {
 
 	/**
@@ -14,7 +40,8 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Orders O1;
+		
 	}
 
 }
