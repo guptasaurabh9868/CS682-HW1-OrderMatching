841ee7e18145516ac0e496bc7fb5b2c52cab9f33
SAURABH GUPTA <guptas@cse.iitb.ac.in>
2018-02-21 03:10:03 +0530

    Added Bug

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	 *///Main Function
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char choice = 'n';
+		char choice = 'n\0';
 		int i = 0;
 		OrderProcessing orderProcessing = new OrderProcessing();
 		do {
